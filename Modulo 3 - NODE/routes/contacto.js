d4554ff55d244b6c3d85b44b3e6ce0dbb6f11a16
Maximiliano Juarez <maximiliano.juarez05@gmail.com>
2021-09-26 19:27:17 -0300

    acomodando todo

---
+++ Modulo 3 - NODE/routes/contacto.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('contacto');
+});
+
+router.post('/', async (req, res, next)=> {
+
+    var nombre = req.body.nombre;
+    var apellido = req.body.apellido;
+    var email = req.body.email;
+    var telefono = req.body.tel;
+    var mensaje =req.body.coment;
+  
+    console.log(req.body);
+  
+    var obj = {
+  
+      to: 'maximiliano.juarez05@gmail.com',
+      subject: 'Contacto desde la web',
+      html: nombre + " " + apellido + " " + " dejo un este comentario en tu sitio web: "  + coment,
+  
+  
+  }
+  
+    var transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+      }
+    })
+    
+    var info = await transporter.sendMail(obj);
+  
+    res.render('contacto', {
+      message: 'Mensaje enviado correctamente',
+    });
+  
+  });
+  
+  module.exports = router;
