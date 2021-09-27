de0da63116fdbb5425227d2c8ec9f8e2b19ca68a
Maximiliano Juarez <maximiliano.juarez05@gmail.com>
2021-09-26 23:58:04 -0300

    problemitas

---
+++ M3 - U4/app.js
@@ -0,0 +1,69 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var saludoRouter = require('./routes/saludo');
+
+var app = express();
+
+var session = require('express-session');
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'ahsjkasjkhkajhs',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.get('/',function (req,res){
+  var conocido = Boolean(req.session.nombre);
+
+  res.render('index', {
+    title: 'Sesiones!!!',
+    conocido: conocido,
+    nombre: req.session.nombre
+  })
+});
+
+app.post('/saludo', function (req, res) {
+  if (req.body.nombre) {
+    req.session.nombre = req.body.combre
+  }
+  res.redirect('/');
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+//app.use('./saludo', saludoRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
