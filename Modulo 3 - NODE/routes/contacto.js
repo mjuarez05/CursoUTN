6d20743707bc54479d63303d5d683b39e7538bbf
Maximiliano Juarez <maximiliano.juarez05@gmail.com>
2021-09-26 23:00:43 -0300

    Ahora si

--- Modulo 3 - NODE/routes/contacto.js
+++ Modulo 3 - NODE/routes/contacto.js
@@ -1,46 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var nodemailer = require('nodemailer');
+var nodemailer = require("nodemailer");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('contacto');
+router.get("/", function (req, res, next) {
+  res.render("contacto");
 });
 
-router.post('/', async (req, res, next)=> {
+router.post("/", async (req, res, next) => {
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var telefono = req.body.tel;
+  var coment = req.body.coment;
 
-    var nombre = req.body.nombre;
-    var apellido = req.body.apellido;
-    var email = req.body.email;
-    var telefono = req.body.tel;
-    var mensaje =req.body.coment;
-  
-    console.log(req.body);
-  
-    var obj = {
-  
-      to: 'maximiliano.juarez05@gmail.com',
-      subject: 'Contacto desde la web',
-      html: nombre + " " + apellido + " " + " dejo un este comentario en tu sitio web: "  + coment,
-  
-  
+  console.log(req.body);
+
+  var obj = {
+    to: "maximiliano.juarez05@gmail.com",
+    subject: "Contacto desde la web",
+    html:
+      nombre +
+      " " +
+      apellido +
+      " " +
+      " dejo un este comentario en tu sitio web: " +
+      coment + "<br></br> Podes contactarlo con los siguientes datos: <br></br>" +
+      email + "<br></br>" +
+      telefono
   }
-  
-    var transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-      }
-    })
-    
-    var info = await transporter.sendMail(obj);
-  
-    res.render('contacto', {
-      message: 'Mensaje enviado correctamente',
-    });
-  
+
+  var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
   });
-  
-  module.exports = router;
+
+  var info = await transporter.sendMail(obj);
+
+  res.render("contacto", {
+    message: "Mensaje enviado correctamente",
+  });
+});
+
+module.exports = router;
