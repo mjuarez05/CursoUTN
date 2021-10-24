e287d9a516afc18ae515e8a93bc35cc82118863c
Maximiliano Juarez <maximiliano.juarez05@gmail.com>
2021-10-24 20:30:13 -0300

    Tarea conexion db

--- Modulo 4/Unidad 3/app.js
+++ Modulo 4/Unidad 3/app.js
@@ -3,16 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session');
-
 
 require('dotenv').config();
 
+var pool = require('./models/bd');
+pool.query("select * from empleados").then(function(resultados){
+  console.log(resultados);
+})
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var contactoRouter = require('./routes/contacto');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,15 +26,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: 'Inserte su contrasena',
-  resave: false,
-  saveUninitialized: true
-}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/contacto', contactoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
