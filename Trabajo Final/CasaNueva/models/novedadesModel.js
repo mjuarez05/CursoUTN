c067d414dc8b8cb6572e86466080d19704ff4500
Maximiliano Juarez <maximiliano.juarez05@gmail.com>
2021-11-19 06:04:51 -0300

    agrega novedad

--- Trabajo Final/CasaNueva/models/novedadesModel.js
+++ Trabajo Final/CasaNueva/models/novedadesModel.js
@@ -6,4 +6,15 @@ async function getNovedades() {
     return rows;
 }
 
-module.exports = {getNovedades}
+async function insertNovedad(obj) {
+    try {
+        var query = "insert into novedades set ? ";
+        var rows = await pool.query(query, [obj]);
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+module.exports = {getNovedades, insertNovedad}
