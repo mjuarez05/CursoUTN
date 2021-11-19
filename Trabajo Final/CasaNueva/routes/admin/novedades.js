42ae4ef2b5a9a209353f337fa9a8a6632d19df95
Maximiliano Juarez <maximiliano.juarez05@gmail.com>
2021-11-18 21:17:10 -0300

    mejora

--- Trabajo Final/CasaNueva/routes/admin/novedades.js
+++ Trabajo Final/CasaNueva/routes/admin/novedades.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
   res.render('admin/novedades', {
       layout:'admin/layout',
-      usuario: req.session.nombre
+      usuario: req.session.nombre,
+      novedades
   });
 });
 
