9584dd33faee19a1adbb677b78000e38b7c21242
Maximiliano Juarez <maximiliano.juarez05@gmail.com>
2021-11-29 16:31:19 -0300

    modificar

--- Trabajo Final/CasaNueva/models/novedadesModel.js
+++ Trabajo Final/CasaNueva/models/novedadesModel.js
@@ -1,20 +1,35 @@
-var pool = require('./bd');
+var pool = require("./bd");
 
 async function getNovedades() {
-    var query = "select * from novedades order by id desc";
-    var rows = await pool.query(query);
-    return rows;
+  var query = "select * from novedades order by id desc";
+  var rows = await pool.query(query);
+  return rows;
 }
 
 async function insertNovedad(obj) {
-    try {
-        var query = "insert into novedades set ? ";
-        var rows = await pool.query(query, [obj]);
-        return rows;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+  try {
+    var query = "insert into novedades set ? ";
+    var rows = await pool.query(query, [obj]);
+    return rows;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
-module.exports = {getNovedades, insertNovedad}
+async function getNovedadById(id) {
+  var query = "update novedades set ? where d=?";
+  var rows = await pool.query(query, [id]);
+  return rows[0];
+}
+
+async function modificarNovedadById(obj, id) {
+  try {
+    var query = "update novedades set ? where id=?";
+    var rows = await pool.query(query, [obj, id]);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+module.exports = { getNovedades, insertNovedad, getNovedadById, modificarNovedadById};
--- Trabajo Final/CasaNueva/routes/admin/novedades.js
+++ Trabajo Final/CasaNueva/routes/admin/novedades.js
@@ -38,4 +38,32 @@ router.post('/agregar', async (req, res, next) => {
   }
 });
 
+router.get('/modificar/:id', async (req, res, next) => {
+  let id = req.params.id;
+  let novedad = await novedadesModel.getNovedadById(id);
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  });
+});
+
+router.post('/modificar', async (req, res, next) => {
+  try {
+    let obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+  }
+  catch(error) {
+    console.log(error)
+    res.render('/admin/modificar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se modifico la novedad'
+    })
+  }
+})
+
 module.exports = router;
